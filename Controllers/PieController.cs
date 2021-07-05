da56dfa67a9bd02b8072dfb3d8d04683ad70d13b
Yousef Aly <youssefali00286@gmail.com>
2021-07-05 10:14:17 +0200

    Added Detail feature

--- Controllers/PieController.cs
+++ Controllers/PieController.cs
@@ -24,5 +24,14 @@ namespace BethanyPieShop.Controllers
             pieListViewModel.Pies = _pieRepository.AllPies;
             return View(pieListViewModel);
         }
+        public IActionResult Detail(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if(pie == null)
+            {
+                return NotFound();
+            }
+            return View(pie);
+        }
     }
 }
